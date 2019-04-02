77c3ebece1b0818258de401ffcb57a1193583930
Zhong Jing <zhongjinggz@163.com>
2019-04-02 17:59:45 +0800

    how to mock legacy code

--- cashregister/answer/src/main/java/tdd/cashregister/CashRegisterLegacy.java
+++ cashregister/answer/src/main/java/tdd/cashregister/CashRegisterLegacy.java
@@ -3,11 +3,14 @@ package tdd.cashregister;
 public class CashRegisterLegacy {
 
     public void process(Purchase purchase) {
-        Printer printer = buildPrinter(); //Extract a method for 'new Printer'
+
+        //Printer printer = new Printer();
+        Printer printer = buildPrinter();
 
         int discount = calculateDiscount(purchase);
         printer.print(purchase.description() + "\ndiscount: " + discount);
     }
+
 
     Printer buildPrinter() {
         return new Printer();
